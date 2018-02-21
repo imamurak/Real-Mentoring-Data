134fb43e902c5af8affe0a2cb9504c3939314cce
Kirk Imamura <kirk.kokoro@gmail.com>
2018-02-21 12:16:17 -0500

    Initial pass at aggregating data for reports

--- Mentoring Data Analysis.R
+++ Mentoring Data Analysis.R
@@ -1,6 +1,7 @@
 getwd()
 
 library(dplyr)
+library(ggplot2)
 
 # Treat zipcode as string for NJ & CT zip codes
 
@@ -120,5 +121,113 @@ sum(FY16eval$se_event_type_desc=="Update")
 FY16eval <- FY16eval[!(FY16eval$se_event_type_desc=="None" | FY16eval$se_event_type_desc=="Update"), ]
 dim(FY16eval)
 
+str(FY16eval, list.len=ncol(FY16eval))
+summary(FY16eval)
+
 # ----------------------------------------------------
 
+# ----- Chapter Stats ------
+
+# Total number of sessions
+length(unique(FY16eval$se_nid))
+
+# Total number of unique clients
+length(unique(FY16eval$se_client_nid))
+
+# Total number of mentoring hours for all counselors involved
+sum(FY16eval$se_hours_spent_total)
+
+# Total number of mentoring hours for lead counselors
+sum(FY16eval$se_hours_spent)
+
+# Number of counselors involved
+length(unique(FY16eval$se_volunteer_nid))
+
+# Co-mentoring stats
+sum(FY16eval$se_morethanone_flag=="yes")
+sum(FY16eval$se_morethanone_flag=="no")
+
+# Mentoring sessions by chapter location
+aggregate(se_client_nid ~se_chapter_loc_name, FY16eval, function(x) length(unique(x)))
+
+# Mentoring sessions by session type
+aggregate(se_client_nid ~se_session_type, FY16eval, function(x) length(unique(x)))
+
+# Mentoring sessions by counseling type
+aggregate(se_client_nid ~se_counseling_type, FY16eval, function(x) length(unique(x)))
+
+# Number of sessions per unique client
+aggregate(se_nid ~se_client_nid, FY16eval, function(x) length(unique(x)))
+
+# Number of average mentoring hours per unique client
+aggregate(se_client_nid ~se_hours_spent, FY16eval, function(x) length(unique(x)))
+
+# Number of sessions by month
+aggregate(se_nid ~month_name, FY16eval, function(x) length(unique(x)))
+
+# Number of sessions by day of week
+aggregate(se_nid ~day_name_of_week, FY16eval, function(x) length(unique(x)))
+
+# Number of total mentoring hours by unique client
+
+# Number of sessons by unique client
+
+
+# ----- Client Demographic Stats -----
+
+# Unique client by state
+aggregate(se_client_nid ~cl_location_state, FY16eval, function(x) length(unique(x)))
+
+# Should filter zip code that is only 5 characters long
+# Unique client by zip code
+aggregate(se_client_nid ~cl_location_zipcode, FY16eval, function(x) length(unique(x)))
+
+# Unique client by gender
+aggregate(se_client_nid ~cl_gender, FY16eval, function(x) length(unique(x)))
+
+# Unique client by age
+aggregate(se_client_nid ~cl_age, FY16eval, function(x) length(unique(x)))
+
+# Unique client by race
+aggregate(se_client_nid ~cl_race, FY16eval, function(x) length(unique(x)))
+
+# Unique client by Latino/Hispanic ethnicity
+aggregate(se_client_nid ~cl_ethnicity_value, FY16eval, function(x) length(unique(x)))
+
+# Unique client by business type
+aggregate(se_client_nid ~cl_business_type_desc, FY16eval, function(x) length(unique(x)))
+
+# Unique client by home based business
+aggregate(se_client_nid ~cl_homebased_value, FY16eval, function(x) length(unique(x)))
+
+# Unique client by legal entity
+aggregate(se_client_nid ~cl_legalentity_value, FY16eval, function(x) length(unique(x)))
+
+# Unique client by how they heard about SCORE
+temp <- aggregate(se_client_nid ~cl_hear_about_score_desc, FY16eval, function(x) length(unique(x)))
+temp
+temp <- temp[order(-temp$se_client_nid), ]
+temp
+
+# dataset[, order(item)]
+
+# FY16eval %>%
+#   group_by(cl_business_type_desc) %>%
+#   summarise(n_distinct(se_client_nid))
+
+
+
+# ----------------------------------------------------
+
+# install.packages(c("ggplot2", "devtools", "dplyr", "stringr"))
+# install.packages(c("maps", "mapdata", "ggmap"))
+
+states <- map_data("state")
+dim(states)
+tristate <- subset(states, region %in% c("new york", "new jersey", "connecticut"))
+
+ggplot(data = tristate) + 
+  geom_polygon(aes(x = long, y = lat, group = group), fill = "palegreen", color = "black") + 
+  coord_fixed(1.3)
+
+eb2 + coord_fixed(xlim = c(-123, -121.0),  ylim = c(36, 38), ratio = 1.3)
